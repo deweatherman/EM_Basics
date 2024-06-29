90d541341ccccb8254c30550eff61a5090c2ff81
deweatherman <deweatherman@hotmail.com>
2024-06-29 10:43:27 +0200

    Using Pandas DataFrames in place of plane unnamed data; using rfft instead of fft (our signals in time domain are real); using amplitude spectral density instead of plain fft to plot spectral content

--- core/Waveform_Basics.py
+++ core/Waveform_Basics.py
@@ -6,6 +6,7 @@
 
 import numpy as np
 import scipy
+import pandas as pd
         
 
 def uerf(t,ta,tb,td,tf):
@@ -17,25 +18,7 @@ def uerf(t,ta,tb,td,tf):
     arg2 = (t-ta)/(0.55*tf)
     result = 0.5*(scipy.special.erf(arg2) - scipy.special.erf(arg1)) 
     return result
-    
-def spectrumRealSignal(n, fft_E):
-    """spectrumRealSignal returns an array with the two-sided spectrum reconstructed
-    from the one-sided version (for real signals in the time domain).
-    The ordering and components are assumed as from:
-    https://numpy.org/doc/stable/reference/routines.fft.html#implementation-details"""
-
-    if(n%2==0):
-        # works for even "n"
-        fft_E_fminus = np.flip(np.conjugate(fft_E[1:int(n/2)]))  #  A[n/2+1:] negative freqs in decreasing neg. freq. 
-        fft_all = np.concatenate((np.append(fft_E[0:int(n/2)], fft_E[int(n/2)]), fft_E_fminus))  #  cat(A[0], A[1:n/2]), A[1:n/2] pos. freqs.  
-        #print(fft_all.shape)
-    else:
-        # Not tested
-        print('Warning: if n odd, this might not work yet')
-        fft_E_fminus = np.flip(np.conjugate(fft_E[1:int((n+1)/2)])) 
-        fft_all = np.concatenate((fft_E[0:int((n-1)/2)+1], fft_E_fminus)) 
-        
-    return fft_all    
+      
         
 class waveForm:
     """The waveForm class contains the definition of a waveform; moreover
@@ -76,17 +59,15 @@ class waveForm:
         if type(pathTocsv) != str:
             raise TypeError("Path to signal file must be a string")
             
-        array = np.genfromtxt(pathTocsv, delimiter=",")   # Read from comma delimited csv
+        array = pd.read_csv(pathTocsv, delimiter=",", on_bad_lines='skip') # Read from comma delimited csv, creates Pandas DataFrame 
         
-        if (array.shape[1]!=2):
+        if (len(array.columns)!=2):
             raise TypeError("Waveform can only be defined with time \
                              and amplitude (2 arrays); might be extended in the future.")
             
-        if (len(array[:,0]) != len(array[:,1])):
-            raise TypeError("Length of time and amplitude arrays must be the same.")
             
-        return cls(time = (array[:,0]-array[0,0]), 
-                   amplitude = array[:,1],
+        return cls(time = array.iloc[:,0].to_numpy(), 
+                   amplitude = array.iloc[:,1].to_numpy(),
                    timeMultiplier = timeMultiplier, 
                    amplitudeMultiplier = amplitudeMultiplier)  # time is refered to "zero"   
 
@@ -104,30 +85,4 @@ class waveForm:
         
         
  
-# Creating class with multiple constructors in Python
-# circle.py
-
-#import math
-#
-#class Circle:
-#    def __init__(self, radius):
-#        self.radius = radius
-#
-#    @classmethod
-#    def from_diameter(cls, diameter):
-#        return cls(radius=diameter / 2)
-
-    # ...
-# And then:
-
-
-#>> from circle import Circle
-#
-#>> Circle.from_diameter(84)
-#Circle(radius=42.0)
-#
-#>> circle.area()
-#5541.769440932395
-#>> circle.perimeter()
-#263.89378290154264
  
